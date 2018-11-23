c72f600c0b5f3e03aca36e899b75f63e152462f3
COSHW <mag.popov@yandex.ru>
2018-11-23 20:26:04 +0300

    lest

--- Try.py
+++ Try.py
@@ -4,9 +4,14 @@ import flask
 app = flask.Flask(__name__)
 
 
-@app.route("/<string:what>")
+@app.route("/<string:what>", methods=["GET"])
 def index(what):
     return what
+
+
+@app.route("/<string:no>", methods=["POST"])
+def index2(no):
+    return no
 
 
 app.run()
