dc9967eb8ba4b58fc987224f18e658744e6ce58a
COSHW <roman-coshw@yandex.ru>
2019-04-13 23:56:14 +0300

    cmon

--- Try.py
+++ Try.py
@@ -14,7 +14,7 @@ def welcome():
 
 @app.route("/result")
 def nextPage():
-    print(flask.request.form.items.to_dict())
+    print(flask.request.values.to_dict())
     #if flask.request.values.to_dict()
     #    info = pandas.DataFrame(requests.get("https://romanrestplz.herokuapp.com/tools/db/maintain"))
     #return flask.render_template("index2.html", table=info.to_html(index=False))
